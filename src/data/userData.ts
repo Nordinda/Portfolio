ee4ed78604bf209533cf834bd8dbe2c3b2caa811
Nordinda <113419171+Nordinda@users.noreply.github.com>
2025-12-15 00:25:34 +0100

    url updated

--- src/data/userData.ts
+++ src/data/userData.ts
@@ -2,5 +2,5 @@ export const userData = {
     name: "Noureddine",
     mail: "nordin.darraz02@gmail.com",
     github: "https://github.com/",
-    linkedin: "https://www.linkedin.com/i",
+    linkedin: "https://www.linkedin.com/in/noureddinedarraz"
 };
